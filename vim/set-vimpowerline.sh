905ac52c86d1e8aad740d491e92f14bbac63ddac
Nemo <526348174@qq.com>
2022-06-14 01:25:31 +0800

    fix syntax error

--- vim/set-vimpowerline.sh
+++ vim/set-vimpowerline.sh
@@ -1,10 +1,10 @@
-#/usr/bin/env zsh
+#!/usr/bin/env zsh
 
 pip3 install powerline-status
 
 vimpwl=$(pip3 show powerline-status | grep 'Location' | sed 's/Location: //')
-vimloc="powerline/bindings/vim"
-if [[ ! -f ~/.vimrc_local]]; then 
+vimloc="/powerline/bindings/vim"
+if [[ ! -f ~/.vimrc_local ]]; then 
     touch ~/.vimrc_local
 fi
 
